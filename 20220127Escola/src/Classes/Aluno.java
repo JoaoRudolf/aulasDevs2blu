dfe114a5507a002f57b26f8e7c69700ff3adda67
JoaoRudolf <66043053+JoaoRudolf@users.noreply.github.com>
2022-02-03 22:01:25 -0300

    Add files via upload

--- 20220127Escola/src/Classes/Aluno.java
+++ 20220127Escola/src/Classes/Aluno.java
@@ -3,12 +3,15 @@ package Classes;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import Util.Conexao;
 
 public class Aluno {
-	
+
 	private int codAluno;
 	private String nomAluno;
 	private String nomMae;
@@ -16,15 +19,20 @@ public class Aluno {
 	private Date dataNasc;
 	private Double medAluno;
 	private String sitAluno;
-	
 
-	// construtores;
-	
+//___________ construtores_______________________
+
 	public Aluno() {
-    }
-	
-	
-	
+	}
+
+	public Aluno(int codAluno, String nomAluno, String nomMae, String nomPai) {
+		super();
+		this.codAluno = codAluno;
+		this.nomAluno = nomAluno;
+		this.nomMae = nomMae;
+		this.nomPai = nomPai;
+	}
+
 	public Aluno(int codAluno, String nomAluno, String nomMae, String nomPai, Date dataNasc, Double medAluno,
 			String sitAluno) {
 		super();
@@ -36,9 +44,8 @@ public class Aluno {
 		this.medAluno = medAluno;
 		this.sitAluno = sitAluno;
 	}
-	
-	public Aluno(String nomAluno, String nomMae, String nomPai, Date dataNasc, Double medAluno,
-			String sitAluno) {
+
+	public Aluno(String nomAluno, String nomMae, String nomPai, Date dataNasc, Double medAluno, String sitAluno) {
 		super();
 		this.nomAluno = nomAluno;
 		this.nomMae = nomMae;
@@ -48,12 +55,9 @@ public class Aluno {
 		this.sitAluno = sitAluno;
 	}
 
+//____________getters e setters____________________
 
-
-
-	// getters e setters?
-	
-    public int getCodAluno() {
+	public int getCodAluno() {
 		return codAluno;
 	}
 
@@ -109,41 +113,53 @@ public class Aluno {
 		this.sitAluno = situacao;
 	}
 
+//___________métododos___________________
 
-	// métodod
-
-
-	public boolean incluirAluno() {
-    	
-    	
-    	
-    	
-		return true;
-    }
-
-    public void alterarAluno() {
-        // TODO implement here
-    }
-
-    public void excluirAluno() {
-        // TODO implement here
-    }
-
-    public void consultarAluno() {
-        // TODO implement here
-    }
-
-    public void listarAlunos() {
-        // TODO implement here
-    }
-
-
-
-    public void salvar() {
+	public void update(int id, String nomAluno, String nomMae, String nomPai) {
 		Connection con = Conexao.conectar();
 
 		String sql = """
-				insert into aluno(nomaluno, nommae, nompai, dataNasc, medaluno, sitaluno) 
+				update aluno 
+				set nomaluno = ?,  nommae = ?, nompai = ?
+				where codaluno = ?
+			
+				""";
+
+		try {
+			PreparedStatement stm = con.prepareStatement(sql);
+			stm.setString(1, nomAluno);
+			stm.setString(2, nomMae);
+			stm.setString(3, nomPai);
+//			stm.setDate(4, this.dataNasc);
+//			stm.setDouble(5, this.medAluno);
+//			stm.setString(6, this.sitAluno);
+			stm.setInt(4, id);
+			stm.execute();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	} // fim do método update
+	
+	public boolean excluir(int id) {
+		Connection con = Conexao.conectar();
+
+		String sql = "delete from aluno where codaluno = ?";
+
+		try {
+			PreparedStatement stm = con.prepareStatement(sql);
+			stm.setInt(1, id);
+			stm.execute();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return true;
+	} // fim do método excluir
+
+	public boolean salvar() {
+		Connection con = Conexao.conectar();
+
+		String sql = """
+				insert into aluno(nomaluno, nommae, nompai, dataNasc, medaluno, sitaluno)
 				values (?,?,?,?,?,?)
 				""";
 
@@ -158,7 +174,59 @@ public class Aluno {
 			stm.execute();
 		} catch (SQLException e) {
 			e.printStackTrace();
+			return false;
 		}
+		return true;
 	} // fim do método salvar
+
+	public List<Aluno> consultaTodos() {
+		List<Aluno> alunos = new ArrayList<>();
+		String sql = "select * from aluno";
+		try {
+			PreparedStatement stm = Conexao.conectar().prepareStatement(sql);
+			ResultSet rs = stm.executeQuery();
+
+			while (rs.next()) {
+				Aluno aluno = new Aluno();
+				aluno.setCodAluno(rs.getInt("codaluno"));
+				aluno.setNomAluno(rs.getString("nomAluno"));
+				aluno.setNomMae(rs.getString("nomMae"));
+				aluno.setNomPai(rs.getString("nomPai"));
+				aluno.setDataNasc(rs.getDate("dataNasc"));
+				aluno.setMedAluno(rs.getDouble("medaluno"));
+				aluno.setSitAluno(rs.getString("sitAluno"));
+				alunos.add(aluno);
+
+			}
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
+		return alunos;
+	} // fim do consulta todos
+	
+	public Aluno consultaPeloId(int id) {
+		Aluno aluno = new Aluno();
+		String sql = "select * from aluno where codaluno = " + id;
+		try {
+			PreparedStatement stm = Conexao.conectar().prepareStatement(sql);
+			ResultSet rs = stm.executeQuery();
+			if (rs.next()) {
+				aluno.setCodAluno(rs.getInt("codaluno"));
+				aluno.setNomAluno(rs.getString("nomaluno"));
+				aluno.setNomMae(rs.getString("nommae"));
+				aluno.setNomPai(rs.getString("nompai"));
+				aluno.setDataNasc(rs.getDate("datanasc"));
+				aluno.setMedAluno(rs.getDouble("medaluno"));
+				aluno.setSitAluno(sql);				
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return aluno;
+		
+	} // fim do consultar por id
 
 }
--- 20220127Escola/src/Classes/MenuPrincipal.java
+++ 20220127Escola/src/Classes/MenuPrincipal.java
@@ -3,6 +3,7 @@ package Classes;
 import java.awt.HeadlessException;
 import java.sql.Date;
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JOptionPane;
 
@@ -11,13 +12,32 @@ import Util.Conexao;
 public class MenuPrincipal {
 
 	public static void main(String[] args) {
-		int opcao = 0; 
+		int opcao = 0;
 		while (opcao != 9) {
 			opcao = telaInicial();
-		
-//________________CREATE_____________________________________|
-			
+
+//_________________________READ ALL_________________________________________________________
+
+			if (opcao == 1) {
+				lerTodos();
+			} // fim do read
+
+//___________FIM DO READ ALL_________________________________________________________________
+
+//_________________________READ BY ID_________________________________________________________
+
 			if (opcao == 2) {
+				String codStr = JOptionPane.showInputDialog("Digite o código do aluno: \n");
+				int codInte = Integer.parseInt(codStr);
+				consultarPorcod(codInte);
+				;
+			} // fim do read
+
+//___________FIM DO READ ALL_________________________________________________________________
+
+//_______________________CREATE__________________________________________________________
+
+			if (opcao == 3) {
 				String nome = null, nommae = null, nompai = null, dataNascimento = null;
 				Double media = 0.0;
 				String situacao = "C";
@@ -36,77 +56,108 @@ public class MenuPrincipal {
 					}
 				} // end while
 				erro = true;
-				
-				criarAluno(nome, nommae, nompai, Date.valueOf(dataNascimento), media, situacao );
+
+				criarAluno(nome, nommae, nompai, Date.valueOf(dataNascimento), media, situacao);
 				System.out.println("Cliente " + nome + " criado com sucesso!");
-			} 
-			//________________FIM DO CREATE_____________________________________||
-		
-		
-		
+			}
+//____________________________FIM DO CREATE________________________________________________________
+
+//_____________________________UPDATE________________________________________||
+
+			if (opcao == 4) {
+				String idStr = JOptionPane.showInputDialog("Digite o codigo do aluno: \n");
+				int id = Integer.parseInt(idStr);
+
+				String nome = null, nommae = null, nompai = null;
+				boolean erro = true;
+
+				while (erro == true) {
+					try {
+						nome = JOptionPane.showInputDialog("Digite o novo nome do aluno de id " + id + ": \n");
+						nommae = JOptionPane.showInputDialog("\"Digite o novo nome da mãe do aluno de id " + id + ": \n");
+						nompai = JOptionPane.showInputDialog("\"Digite o novo nome do pai do cliente de id " + id + ": \\n");
+						erro = false;
+					} catch (HeadlessException e) {
+						e.printStackTrace();
+						erro = true;
+					}
+				} // end while
+				erro = true;
+
+				chamaUpdate(id, nome, nommae, nompai);
+
+				System.out.println("Cliente de id " + id + " alterado com sucesso!");
+			}
+
+// ________________________FIM DO UPDATE_____________________________________||
+			
+			
+//________________DELETE_____________________________________|
+			
+			if (opcao == 5) {
+				String idStr = JOptionPane.showInputDialog("Digite o id do aluno a ser excluído: \n");
+				int id = Integer.parseInt(idStr);
+				chamaExcluir(id);
+			}
+			
+			
+
 //		String dataNascimento = "2000-02-01";
 //		criarAluno("a", "b", "c", Date.valueOf(dataNascimento), 0.0, "d");
-		
-		
-		
+
 		} // FIM DO WHILE DA TELA INICIAL
-		
-		
+
 //		Calendar c = Calendar.getInstance();
 //		Date data = c.getTime();
 //		
 //		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 //		System.out.println("Data formatada: " + sdf.format(data));
-		
+
 //		Aluno alu = new Aluno();
 //		String dataNascimento = "2000-02-01";
 //		
 //		alu.setDataNasc(Date.valueOf(dataNascimento));
 //		// alu.mostrarData();
 //		System.out.println("Data de nascimento informada " + dataNascimento);
-		
-		
 
 	} // FIM DO MAIN__________________________
-	
-		
-		private static int telaInicial() {
-			ArrayList<Integer> opcoes = new ArrayList<>();
-			opcoes.add(1);
-			opcoes.add(2);
-			opcoes.add(3);
-			opcoes.add(4);
-			opcoes.add(9);
 
-			int telaA = 0;
-			boolean erro = true;
+	private static int telaInicial() {
+		ArrayList<Integer> opcoes = new ArrayList<>();
+		opcoes.add(1);
+		opcoes.add(2);
+		opcoes.add(3);
+		opcoes.add(4);
+		opcoes.add(5);
+		opcoes.add(9);
 
-			while (erro == true) {
-				try {
-					String s = JOptionPane.showInputDialog("Selecione a opção: \n" + "1 para consultar os alunos \n"
-							+ "2 para inserir novo aluno \n" + "3 para atualizar um aluno \n"
-							+ "4 para excluir um aluno \n" + "9 para sair do sistema");
-					telaA = Integer.parseInt(s);
-					System.out.println(telaA);
-					if (opcoes.contains(telaA)) {
-						erro = false;
-					}
-				} catch (HeadlessException e) {
-					e.printStackTrace();
-					erro = true;
-				} catch (NumberFormatException e) {
-					e.printStackTrace();
-					erro = true;
+		int telaA = 0;
+		boolean erro = true;
+
+		while (erro == true) {
+			try {
+				String s = JOptionPane.showInputDialog("Selecione a opção: \n"
+						+ "1 para consultar todos os alunos cadastrados \n" + "2 para consultar um aluno por id \n"
+						+ "3 para inserir novo aluno \n" + "4 para atualizar um aluno \n" + "5 para excluir um aluno \n"
+						+ "9 para sair do sistema");
+				telaA = Integer.parseInt(s);
+				if (opcoes.contains(telaA)) {
+					erro = false;
 				}
-			} // end while
-			erro = true;
-			return telaA;
+			} catch (HeadlessException e) {
+				e.printStackTrace();
+				erro = true;
+			} catch (NumberFormatException e) {
+				e.printStackTrace();
+				erro = true;
+			}
+		} // end while
+		erro = true;
+		return telaA;
 
-		} // fim tela inicial
-	
-	
+	} // fim tela inicial
 
-	private static void criarAluno(String nome, String nomMae, String nomPai, Date dataNasc, Double media, String situacao) {
+	static void criarAluno(String nome, String nomMae, String nomPai, Date dataNasc, Double media, String situacao) {
 		Aluno aluno = new Aluno();
 		aluno.setNomAluno(nome);
 		aluno.setNomMae(nomMae);
@@ -118,30 +169,46 @@ public class MenuPrincipal {
 
 	}
 
-//	static void lerTodos() {
-//		List<Aluno> listaAluno = new Aluno().consultaTodos();
-//
-//		for (Aluno c : listaAluno) {
-//			System.out.println(c.getId());
-//			System.out.println(c.getNome());
-//			System.out.println(c.getCpf());
-//			System.out.println(c.getEndereco());
-//			System.out.println(c.getTelefone());
-//			System.out.println();
-//		}
-//		
-//		System.out.println("_______ Fim da Consulta atual_______ \n\n");
-//
-//	}
-//	
-//	static Aluno alterarAluno(int id, String nome, String cpf, String endereco, String telefone) {
-//		Aluno aluno = new Aluno().consultaPeloId(id);
-//		aluno.setNomAluno(nome);
-//		aluno.setCpf(cpf);
-//		aluno.setEndereco(endereco);
-//		aluno.setTelefone(telefone);
-//		return aluno;
-//	}
+	static void lerTodos() {
+		List<Aluno> listaAluno = new Aluno().consultaTodos();
+		System.out.println("Cod   | nome     |    nome mãe   |    nome pai |");
+
+		for (Aluno a : listaAluno) {
+			System.out.printf("%d 	%s	 	 %s 	%s	\n", a.getCodAluno(), a.getNomAluno(), a.getNomMae(),
+					a.getNomPai());
+		}
+		System.out.println();
+
+		System.out.println("_______ Fim da Consulta atual_______ \n\n");
+
+	}
+
+	static void consultarPorcod(int id) {
+		Aluno a = new Aluno().consultaPeloId(id);
+		System.out.println("Cod   | nome     |    nome mãe   |    nome pai |");
+		System.out.printf("%d 	%s	 	 %s 	%s	\n", a.getCodAluno(), a.getNomAluno(), a.getNomMae(),
+				a.getNomPai());
+	}
+
+	static void chamaUpdate(int id, String nomealuno, String nommae, String nompai) {
+		Aluno aluno = new Aluno().consultaPeloId(id);
+		aluno.update(id, nomealuno, nommae, nompai);
+	}
+	
+	static void chamaExcluir(int id) {
+		Aluno aluno = new Aluno();
+		if (aluno.excluir(id)) {
+			System.out.println("excluído com sucesso");
+		}
+	}
+		
+		
+		
+		
+//		contato.setCelular("47 6969-6969");
+//		if (new DaoContato().update(contato)) {
+//			System.out.println("Alterado com sucesso");
+
 //	
 //	static void chamaExcluir(int id) {
 //		Aluno aluno = new Aluno();
@@ -149,8 +216,7 @@ public class MenuPrincipal {
 //			System.out.println("excluído com sucesso");
 //		}
 //	}
-	
-	
+
 	static void testeConexao() {
 		if (Conexao.conectar() != null) {
 			System.out.println("conectado");
@@ -158,7 +224,5 @@ public class MenuPrincipal {
 			System.out.println("erro ao conectar");
 		}
 	}
-	
-	
 
 }
