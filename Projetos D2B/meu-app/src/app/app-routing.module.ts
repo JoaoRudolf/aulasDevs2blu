871fdee5761a6c914979f6013d32bbcb11dbeedc
JoaoRudolf <66043053+JoaoRudolf@users.noreply.github.com>
2022-02-28 22:06:03 -0300

    Add files via upload

---
+++ Projetos D2B/meu-app/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { HomeComponent } from './components/home/home.component';
+import { ContatoComponent } from './components/contato/contato.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'contato', component: ContatoComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
